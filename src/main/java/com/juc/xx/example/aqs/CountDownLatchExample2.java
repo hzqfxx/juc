cdd4bab92e4260466c623695fc12dd86204fce77
xiaoxiang <xiaoxiang0719@126.com>
2018-11-16 09:33:25 +0800

    CountDownLatch

--- src/main/java/com/juc/xx/example/aqs/CountDownLatchExample2.java
+++ src/main/java/com/juc/xx/example/aqs/CountDownLatchExample2.java
@@ -3,17 +3,19 @@ package com.juc.xx.example.aqs;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class CountDownLatchExample1 {
+public class CountDownLatchExample2 {
     private  final static int ThreadCount = 200;
 
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService pool = Executors.newCachedThreadPool();
+        ExecutorService pool = Executors.newFixedThreadPool(10);
         CountDownLatch countDownLatch = new CountDownLatch(ThreadCount);
         for (int i = 0; i < ThreadCount; i++) {
-            final String name = Thread.currentThread().getName();
+
             final int ThreadNum=i;
             pool.execute(()->{
+                final String name = Thread.currentThread().getName();
                 try{
                     test(ThreadNum,name);
                 }catch (Exception e){
@@ -24,12 +26,12 @@ public class CountDownLatchExample1 {
 
             });
         }
-        countDownLatch.await();
+        //超过时间不予等待,直接执行后面操作(但后面的还是回执行完成)
+        countDownLatch.await(1, TimeUnit.MILLISECONDS);
         System.out.println("finish");
     }
 
     private static void test(int ThreadNum,String name) throws InterruptedException {
-        Thread.sleep(10000);
         System.out.println(ThreadNum+"===="+ name);
 
     }
